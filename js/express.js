01793a3f88155c594e819e02dbbe7859327d5d8b
Mukul Agrawal <mukul2310@github.com>
2021-04-11 01:21:36 +0530

    Fix#2

--- js/express.js
+++ js/express.js
@@ -1,2 +1,10 @@
-var express=import('express')
-var app=express();
+var express = require('express');  
+var app = express();  
+app.get('/', function (req, res) {  
+  res.send('Welcome to JavaTpoint!');  
+});  
+var server = app.listen(5500, function () {  
+  var host = server.address().address;  
+  var port = server.address().port;  
+  console.log('Example app listening at http://%s:%s', host, port);  
+});  
