2da95bda03c6c8d7423a4f41a1890f6de059dfe6
Mukul Agrawal <mukul2310@github.com>
2021-04-15 21:08:39 +0530

    new commit

--- js/express.js
+++ js/express.js
@@ -1,10 +1,14 @@
-var express = require('express');  
-var app = express();  
-app.get('/', function (req, res) {  
-  res.send('Welcome to JavaTpoint!');  
-});  
-var server = app.listen(5500, function () {  
-  var host = server.address().address;  
-  var port = server.address().port;  
-  console.log('Example app listening at http://%s:%s', host, port);  
-});  
+let express = require('express');  
+let app = express();
+let port=8080;
+app.listen(port,()=>
+console.log("Server running"));
+
+app.get('/', function (req, res) 
+{
+  res.sendFile('index.html', { root: '.' });
+});
+app.get('/open', function (req, res) 
+{
+  res.redirect(301,"https://bitly.com/");
+});
