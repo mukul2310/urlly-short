f8e2f6959cc8b0e020932ebb4139d167f9b8e7e3
Mukul Agrawal <mukul2310@github.com>
2021-05-01 15:36:33 +0530

    started deployment
    Signed-off-by: Mukul Agrawal <mukul2310@github.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let port=8080;
+let port=process.env.PORT|| 8080;
 app.listen(port,()=>
 console.log("Server running"));
 app.use('/',express.static(__dirname+'/public'));
